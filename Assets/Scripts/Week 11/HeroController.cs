5d83f58331c617258c50ab4756564676ee06a867
KaleWizard <kalewizard.dev@gmail.com>
2025-03-27 14:42:56 -0400

    Finished Footsteps task

---
+++ Assets/Scripts/Week 11/HeroController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    public Animator animator;
+
+    Vector3 gravity = Vector2.down * 4;
+
+    public Vector3 velocity = Vector3.zero;
+
+    float speed = 30f;
+
+    SpriteRenderer sr;
+
+    public List<AudioClip> footsteps;
+
+    public AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        velocity += Vector3.right * Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        velocity.x *= 0.95f;
+
+        velocity += gravity * Time.deltaTime;
+
+        Vector2 screenCorner = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        if (transform.position.y < screenCorner.y)
+        {
+            transform.position = new Vector2(transform.position.x, screenCorner.y);
+            velocity.y = 0f;
+            animator.SetBool("Grounded", true);
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetTrigger("Jump");
+                velocity.y = 3f;
+                animator.SetBool("Grounded", false);
+            }
+        }
+
+        animator.SetFloat("AirSpeedY", velocity.y);
+        if (Mathf.Abs(velocity.x) >= 0.1f)
+        {
+            animator.SetInteger("AnimState", 1);
+        } else
+        {
+            animator.SetInteger("AnimState", 0);
+        }
+
+        sr.flipX = velocity.x < 0;
+
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    public void PlayFootstep()
+    {
+        audio.PlayOneShot(footsteps[Random.Range(0, footsteps.Count)]);
+    }
+}
