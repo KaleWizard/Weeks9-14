027c69684a2cfe93d0c022cf1c1eae6b024b519a
KaleWizard <kalewizard.dev@gmail.com>
2025-03-31 19:38:20 -0400

    Implemented Bomb ability with some bugs

--- Assets/Scripts/System/BombCollision.cs
+++ Assets/Scripts/System/BombCollision.cs
@@ -1,18 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BombCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 velocity;
+
+    public PlayerMovement physics;
+
+    float bombRadius = 0.15f;
+
+    float initialSpeed = 4f;
+
+    public void Throw(Vector2 vel)
     {
-        
+        velocity = vel * initialSpeed;
+        StartCoroutine(BombMovement());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator BombMovement()
     {
-        
+        while (!CollidingWithTerrain())
+        {
+            Vector2 pos = transform.position;
+            velocity += physics.gravity * Time.deltaTime;
+            pos += velocity * Time.deltaTime;
+            transform.position = pos;
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+
+    bool CollidingWithTerrain()
+    {
+        Vector3Int cellPos = physics.terrain.WorldToCell(transform.position);
+
+        // Check top edge
+        if (!IsCellEmpty(cellPos, 0, 1) && (transform.position.y % 1) > 1 - bombRadius)
+            return true;
+        // Check bottom edge
+        if (!IsCellEmpty(cellPos, 0, -1) && (transform.position.y % 1) < bombRadius)
+            return true;
+        // Check left 
+        if (!IsCellEmpty(cellPos, -1, 0) && (transform.position.x % 1) < bombRadius)
+            return true;
+        // Check right edge
+        if (!IsCellEmpty(cellPos, 1, 0) && (transform.position.x % 1) > 1 - bombRadius)
+            return true;
+
+        // Get position of bomb's cell in the world
+        Vector3 cellWorldPos = physics.terrain.CellToWorld(cellPos);
+        cellWorldPos.z = transform.position.z;
+
+        // Check top left corner
+        if (!IsCellEmpty(cellPos, -1, 1) && CollidingWithPoint(cellWorldPos + Vector3.up))
+            return true;
+        // Check top right corner
+        if (!IsCellEmpty(cellPos, 1, 1) && CollidingWithPoint(cellWorldPos + Vector3.up + Vector3.right))
+            return true;
+        // Check bottom left corner
+        if (!IsCellEmpty(cellPos, -1, -1) && CollidingWithPoint(cellWorldPos))
+            return true;
+        // Check bottom right corner
+        if (!IsCellEmpty(cellPos, 1, -1) && CollidingWithPoint(cellWorldPos + Vector3.right))
+            return true;
+
+        // No collisions found, so return false
+        return false;
+    }
+
+    bool IsCellEmpty(Vector3Int cell, int xModifier, int yModifier)
+    {
+        cell.x += xModifier;
+        cell.y += yModifier;
+        TileBase tile = physics.terrain.GetTile(cell);
+        return tile == null;
+    }
+
+    bool CollidingWithPoint(Vector3 point)
+    {
+        return Vector3.Distance(transform.position, point) < bombRadius;
     }
 }
--- Assets/Scripts/System/BomberAbility.cs
+++ Assets/Scripts/System/BomberAbility.cs
@@ -5,23 +5,47 @@ using UnityEngine;
 public class BomberAbility : MonoBehaviour
 {
     AbilityCooldown cooldown;
+    float cooldownLength = 4f;
+
+    bool isHoldingBomb;
+
+    public BombCollision bombPrefab;
+    BombCollision bomb;
+
+    Vector2 direction = Vector2.zero;
+    public Transform indicatorPrefab;
+    Transform indicator;
+
+    Coroutine indicatorUpdate = null;
+
+    PlayerMovement movementScript;
 
     // Called on scene load
     public void Initialize(AbilityCooldown c)
     {
         cooldown = c;
+        movementScript = GetComponent<PlayerMovement>();
     }
 
     // Called upon ability selected
     public void Activate()
     {
-
+        isHoldingBomb = false;
     }
 
     // Called upon ability action used
     public void Use()
     {
-
+        if (isHoldingBomb)
+        {
+            ThrowBomb();
+            isHoldingBomb = false;
+            cooldown.StartTimer(cooldownLength);
+        } else
+        {
+            SpawnBomb();
+            isHoldingBomb = true;
+        }
     }
 
     // Called on cooldown finished
@@ -33,6 +57,40 @@ public class BomberAbility : MonoBehaviour
     // Called when ability deselected
     public void Deactivate()
     {
+        if (bomb != null)
+            Destroy(bomb);
+        if (indicator != null)
+            Destroy(indicator.gameObject);
+        if (indicatorUpdate != null)
+            StopCoroutine(indicatorUpdate);
+    }
 
+    void ThrowBomb()
+    {
+        StopCoroutine(indicatorUpdate);
+        Destroy(indicator.gameObject);
+        bomb.Throw(direction.normalized);
+    }
+
+    IEnumerator UpdateIndicator()
+    {
+        while (true)
+        {
+            if (movementScript.velocity.SqrMagnitude() > 0.05f)
+            {
+                direction = movementScript.velocity;
+            }
+            indicator.up = direction;
+            bomb.transform.position = transform.position;
+            yield return null;
+        }
+    }
+
+    void SpawnBomb()
+    {
+        bomb = Instantiate(bombPrefab, transform.position, Quaternion.identity);
+        bomb.physics = movementScript;
+        indicator = Instantiate(indicatorPrefab, transform);
+        indicatorUpdate = StartCoroutine(UpdateIndicator());
     }
 }
